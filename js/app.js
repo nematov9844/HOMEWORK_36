b612821d36699247b38ddda4f57ff2110fc24ab9
Nematov Ro'zimuhammad <jeki2004114r@gmail.com>
2024-06-17 23:22:43 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+
+
+
+function btn(){
+    let element = document.querySelector(".contact");
+    element.classList.toggle("toggle");
+}
+
+function tuf(){
+    let element = document.querySelector(".dropdown");
+    element.classList.toggle("toggle");
+}
+
